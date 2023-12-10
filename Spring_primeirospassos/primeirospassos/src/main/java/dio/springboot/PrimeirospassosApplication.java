453b8c07c7eb5fae269fb57feea60dd5cfbd1bb8
Drica2023 <adrianamedeiros.dev@outlook.com>
2023-12-10 12:08:20 -0300

    Primeiros passos no SpringBoot

---
+++ Spring_primeirospassos/primeirospassos/src/main/java/dio/springboot/PrimeirospassosApplication.java
@@ -0,0 +1,14 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirospassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirospassosApplication.class, args);
+	}
+
+
+}
