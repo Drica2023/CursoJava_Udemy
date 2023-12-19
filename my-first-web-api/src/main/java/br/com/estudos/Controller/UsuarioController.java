e7515598816fd90891c4577e2d34d318644837b1
Drica2023 <adrianamedeiros.dev@outlook.com>
2023-12-19 12:14:04 -0300

    simulando o retorno dos dados em uma base de dados

---
+++ my-first-web-api/src/main/java/br/com/estudos/Controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package br.com.estudos.Controller;
+
+import br.com.estudos.model.Usuario;
+import br.com.estudos.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class UsuarioController {
+    @Autowired
+         private UsuarioRepository repository;
+        @GetMapping("/users")
+        public List<Usuario> getUsers(){
+            return repository.FindAll();
+        }
+
+
+
+}
+
---
+++ my-first-web-api/src/main/java/br/com/estudos/repository/UsuarioRepository.java
@@ -0,0 +1,42 @@
+package br.com.estudos.repository;
+
+import br.com.estudos.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+   public void save(Usuario usuario){
+       if(usuario.getId()==null)
+           System.out.println("Save - Recebendo o usuario na camada de repositorio");
+       else
+           System.out.println("UPDATE - recebebdo o usuario na camada de repositorio");
+          }
+
+          public void deleteById(Integer id){
+       System.out.println(String.format("DELETE / id - recebendo o id: %d para exclusao"));
+       System.out.println(id);
+
+          }
+
+          public List<Usuario> FindAll(){
+       System.out.println("LIST - Listando os usuarios do sistema");
+       List<Usuario> usuarios = new ArrayList<>();
+       usuarios.add(new Usuario("Adriana","Password"));
+       usuarios.add(new Usuario("Gleyson", "masterpass"));
+       return usuarios;
+
+          }
+
+          public Usuario findById(Integer id){
+       System.out.println(String.format("FIND/ID - Recebebdo o id: %id para localizacao"));
+       return new Usuario("Adriana", "Password");
+          }
+
+    public Usuario findByUsername(String username){
+        System.out.println(String.format("FIND/ID - Recebebdo o id: %id para localizacao"));
+        return new Usuario("Gleyson", "masterpass");
+    }
+}
