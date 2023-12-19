65d713ae3415fd368480021f7cc3fbd1419d4c39
Drica2023 <adrianamedeiros.dev@outlook.com>
2023-12-19 11:07:52 -0300

    My first web api

---
+++ my-first-web-api/src/main/java/br/com/estudos/Controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package br.com.estudos.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to Spring Boot web API  " + "  Hello World";
+    }
+
+}
