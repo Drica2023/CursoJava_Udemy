a33771e61e7b357c4981bf9cf84df4de3b965590
Drica2023 <adrianamedeiros.dev@outlook.com>
2024-01-04 16:53:45 -0300

    implementado a funcionalidade de Listagem dos dados

--- apiRest/src/main/java/Med/Cadastro/apiRest/controller/MedicosController.java
+++ apiRest/src/main/java/Med/Cadastro/apiRest/controller/MedicosController.java
@@ -1,14 +1,18 @@
 package Med.Cadastro.apiRest.controller;
 
 import Med.Cadastro.apiRest.medico.DadosCadastroMedico;
+import Med.Cadastro.apiRest.medico.DadosListagemMedico;
 import Med.Cadastro.apiRest.medico.Medico;
 import Med.Cadastro.apiRest.medico.MedicoReposity;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -17,10 +21,17 @@ public class MedicosController {
     private MedicoReposity reposity;
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
 
         reposity.save(new Medico(dados));
 
     }
+    @GetMapping
+    public Page<DadosListagemMedico> listar( @PageableDefault(sort = {"nome"}) Pageable paginacao) {
+        return reposity.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+
+    
 
 }
